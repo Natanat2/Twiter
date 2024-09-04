e8ce53023864f619ee225c2c56862a966938530e
Natanat2 <natanat2@mail.ru>
2024-09-04 14:11:23 +0500

    add model

--- publicposts/publicpostsapp/models.py
+++ publicposts/publicpostsapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+class Post(models.Model):
+    Text = models.CharField(max_length = 400)
+    likesCount = models.IntegerField(default = 0)
+    date = models.DateTimeField('publish date', auto_now_add = True)
 
-# Create your models here.
+def __str__(self):
+    return self.text
